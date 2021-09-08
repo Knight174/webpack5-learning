fb104fe3af9beae280f4421d43f1095e72298edf
Knight174 <cpc4253@163.com>
2021-09-08 21:31:50 +0800

    删除多余代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,57 @@
-const commonConfig = require('./build2/webpack.common.config.js')
-const productionConfig = require('./build2/webpack.prod.config.js')
-const developmentConfig = require('./build2/webpack.dev.config.js')
-const { merge } = require('webpack-merge');
+const path = require('path');
+// --------------------------
+// 自动生成 html 模板
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-
-module.exports = (env) => {
-  const isProduction = env && env.production
-  const config = isProduction ? productionConfig : developmentConfig
-  return merge(commonConfig, config)
-}
+module.exports = {
+  // 模式
+  mode: 'development',
+  // 性能优化 (tree shaking)
+  optimization: {
+    usedExports: true
+  },
+  // source-map
+  devtool: 'eval-cheap-module-source-map',
+  // 入口文件
+  entry: './src/index.js',
+  // 开发服务器
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: 'only'
+  },
+  // 输出文件
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    assetModuleFilename: 'assets/[name]_[hash][ext]',
+    clean: true,
+  },
+  // 模块
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        sideEffects: true
+      }
+    ]
+  },
+  // 插件
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
