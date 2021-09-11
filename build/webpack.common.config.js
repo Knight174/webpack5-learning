3c07a371d8b3be13b738e6e8ddaa58a77cbfea6c
Knight174 <cpc4253@163.com>
2021-09-11 09:13:06 +0800

    更新

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -36,32 +36,32 @@ module.exports = {
     })
   ],
   // 性能优化
-  optimization: {
-    // 同步代码分割
-    splitChunks: {
-      chunks: 'all', // 同步、异步都会分割
-      minSize: 20000, // 如果模块大小小于这个值，则不会被分割
-      minRemainingSize: 0, // 如果模块大小小于这个值，则不会被分割
-      minChunks: 2, // 如果模块被引用次数小于这个值，则不会被分割
-      maxAsyncRequests: 30, // 异步模块，一次最多被加载的次数
-      maxInitialRequests: 30, // 入口模块最多被加载的次数
-      enforceSizeThreshold: 50000, // 如果模块大小超过这个值，则不会被分割
-      cacheGroups: { // 缓存组
-        // 打包第三方库
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/, // 正则匹配第三方库文件
-          priority: -10, // 优先级
-          reuseExistingChunk: true, // 如果一个模块已经被打包过了，那么这个模块也不会被打包
-          filename: 'vendors.[name].js' // 打包后的文件名
-        },
-        // 打包公共模块
-        default: {
-          minChunks: 2, // 被超过两个模块引用，才会被打包
-          priority: -20, // 优先级
-          reuseExistingChunk: true, // 如果一个模块已经被打包过了，那么这个模块也不会被打包
-          filename: 'common.js' // 打包后的文件名
-        },
-      },
-    },
-  }
+  // optimization: {
+  //   // 同步代码分割
+  //   splitChunks: {
+  //     chunks: 'all', // 同步、异步都会分割
+  //     minSize: 20000, // 如果模块大小小于这个值，则不会被分割 20kb
+  //     minRemainingSize: 0, // 最小可保存大小，开发模式下为 0，其他情况下等于 minSize，一般不用手动配置
+  //     minChunks: 2, // 如果模块被引用次数小于这个值，则不会被分割
+  //     maxAsyncRequests: 30, // 异步模块，一次最多被加载的次数
+  //     maxInitialRequests: 30, // 入口模块最多被加载的次数
+  //     enforceSizeThreshold: 50000, // 强制分割的大小阈值 50kb
+  //     cacheGroups: { // 缓存组
+  //       // 打包第三方库
+  //       defaultVendors: {
+  //         test: /[\\/]node_modules[\\/]/, // 正则匹配第三方库文件
+  //         priority: -10, // 优先级
+  //         reuseExistingChunk: true, // 如果一个模块已经被打包过了，那么这个模块也不会被打包
+  //         filename: 'vendors.[name].js' // 打包后的文件名
+  //       },
+  //       // 打包公共模块
+  //       default: {
+  //         minChunks: 2, // 被超过两个模块引用，才会被打包
+  //         priority: -20, // 优先级
+  //         reuseExistingChunk: true, // 如果一个模块已经被打包过了，那么这个模块也不会被打包
+  //         filename: 'common.js' // 打包后的文件名
+  //       },
+  //     },
+  //   },
+  // }
 };
--- src/index.js
+++ src/index.js
@@ -4,20 +4,23 @@
 // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 // document.body.appendChild(element);
 
+// import test from './assets/js/test.js';
+// console.log(test.name);
+
 // 异步代码
-// function getComponent() {
-//   return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-//     let element = document.createElement('div');
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     return element;
-//   });
-// }
-async function getComponent() {
-  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+    let element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+  });
 }
+// async function getComponent() {
+//   const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+//   let element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+// }
 document.addEventListener('click', () => {
   getComponent().then(element => {
     document.body.appendChild(element);
